e003cfde3f4acf0f5940a121031aab7ecb01d4fd
Omar Berrabeh <omar.berrabeh@ekino.com>
2024-11-06 16:56:49 +0100

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'faq',
+    loadComponent: () => import('./faq/faq.component')
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./home/home.component')
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
